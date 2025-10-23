7ac94f5d5b0263bf9a0538908751b9a0e84e5dd2
Toee Thiha Kyaw <toeethihakyaw@gmail.com>
2025-10-23 22:45:54 +0630

    Fix dashboard UI.

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import { UserContextProvider } from "./components/context/UserContext.jsx";
 import { Toaster } from "react-hot-toast";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.js";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 createRoot(document.getElementById("root")).render(
   <RouteMethod>
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -4,37 +4,61 @@ function Dashboard() {
     <>
       <h1 className="m-2">Dashboard</h1>
       <div className="container">
-        <div className="row p-2">
+        <div className="row p-2 align-items-stretch">
           <div className="col-6 p-2">
             <Link
               to="/dashboard/users/create"
-              className="d-block btn btn-primary fs-5"
+              className="d-block btn btn-primary h-100 d-flex justify-content-center align-items-center"
             >
-              Create New User
+              <div
+                className="d-flex align-items-center gap-3"
+                style={{ fontSize: "1.5rem" }}
+              >
+                <i className="bi bi-person-plus"></i>
+                <span className="d-none d-sm-block">Create User</span>
+              </div>
             </Link>
           </div>
           <div className="col-6 p-2">
             <Link
               to="/dashboard/items/create"
-              className="d-block btn btn-primary fs-5"
+              className="d-block btn btn-primary h-100 d-flex justify-content-center align-items-center"
             >
-              Add Item
+              <div
+                className="d-flex align-items-center gap-3"
+                style={{ fontSize: "1.5rem" }}
+              >
+                <i className="bi bi-clipboard-plus"></i>
+                <span className="d-none d-sm-block">Add Item</span>
+              </div>
             </Link>
           </div>
           <div className="col-6 p-2">
             <Link
               to="/dashboard/items/history"
-              className="d-block btn btn-primary fs-5"
+              className="d-block btn btn-primary h-100 d-flex justify-content-center align-items-center"
             >
-              Histroy
+              <div
+                className="d-flex align-items-center gap-3"
+                style={{ fontSize: "1.5rem" }}
+              >
+                <i className="bi bi-clock-history"></i>
+                <span className="d-none d-sm-block">Histroy</span>
+              </div>
             </Link>
           </div>
           <div className="col-6 p-2">
             <Link
               to="/dashboard/items/search"
-              className="d-block btn btn-primary fs-5"
+              className="d-block btn btn-primary h-100 d-flex justify-content-center align-items-center"
             >
-              Search
+              <div
+                className="d-flex align-items-center gap-3"
+                style={{ fontSize: "1.5rem" }}
+              >
+                <i className="bi bi-search"></i>
+                <span className="d-none d-sm-block">Search</span>
+              </div>
             </Link>
           </div>
         </div>
